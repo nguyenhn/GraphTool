d17ed4ac7a56a5e6199d3fdbb59759a5aefc0a32
Nguyen Hoang Nguỵen <nguyen.nguyenhoang@live.com>
2017-04-06 11:23:28 +0700

    Full graph representing method

--- GraphTool/GraphTool/Graph.cs
+++ GraphTool/GraphTool/Graph.cs
@@ -12,10 +12,16 @@ namespace GraphTool
 	/// Adjs[i] = All edges form nod i
 	/// </summary>
 	public class Arc
-	{	
+	{
 		public int Des { get; set; } //Link to destination vertex
 		public string Type { get; set; }
-		public double Weight { get; set; }		
+		public double Weight { get; set; }
+		public int Label { get; set; }
+		public Arc(int d, int label)
+		{
+			this.Des = d;
+			this.Label = label;
+		}
 		public Arc(int d, double weight)
 		{
 			this.Des = d;
@@ -29,23 +35,55 @@ namespace GraphTool
 		}
 	}
 
+	public class Node
+	{
+		public int Id { get; set; }
+		public int Name { get; set; }
+	}
+	public class Edge
+	{
+		public int Id { get; set; }
+		public int StartNode { get; set; }
+		public int EndNode { get; set; }
+		public int Cost { get; set; }
+		public int Label { get; set; }
+		public Edge(int startNode, int endNode, int id)
+		{
+			this.Id = id;
+			this.StartNode = startNode;
+			this.EndNode = endNode;
+		}
+		public Edge(int startNode, int endNode, int id, int label)
+		{
+			this.Id = id;
+			this.StartNode = startNode;
+			this.EndNode = endNode;
+			this.Label = label;
+		}
+	}
 	// A directed graph using adjacency list representation
 	class Graph
 	{
 		#region Public Attributes		
 		public List<List<int>> AllPathList { get; set; }
-		public List<Tuple<List<int>,double>> AllPathListWithDistance { get; set; }
+		public List<Tuple<List<int>, double>> AllPathListWithDistance { get; set; }
 
-		#endregion		
+		#endregion
 		int V; //Total Vertex of Graph
 		List<int>[] Adjs; // Array of adjacency list
 		List<Arc>[] CustomAdjs; /// Adjs[i] = All edges form node i
+		List<Edge>[] EdgeAdjs; // by Id of edge
+		List<Edge> EdgeList;
 		Queue<int> mainQueue;
 		Stack<int> mainStack;
+		List<Arc>[] PreTrack;
+
 		private void Init()
 		{
 			Adjs = new List<int>[V];
 			CustomAdjs = new List<Arc>[V];
+			EdgeList = new List<Edge>();
+			EdgeAdjs = new List<Edge>[V];
 			//Initial all Adjs list for each vertex
 			AllPathList = new List<List<int>>();
 			AllPathListWithDistance = new List<Tuple<List<int>, double>>();
@@ -53,7 +91,7 @@ namespace GraphTool
 			mainStack = new Stack<int>();
 		}
 		#region public					
-		public Graph (int v)
+		public Graph(int v)
 		{
 			this.V = v;
 			Init();
@@ -61,19 +99,43 @@ namespace GraphTool
 			{
 				Adjs[i] = new List<int>();
 				CustomAdjs[i] = new List<Arc>();
+				EdgeAdjs[i] = new List<Edge>(); 
 			}
 		}
 
 		public void AddEdge(int u, int v)
 		{
-			Adjs[u].Add(v);			
+			Adjs[u].Add(v);
 		}
 
-		public void AddEdge(int u, int v, double w)
+		public void AddEdge(int u, int v, int Id)
 		{
-			Arc newEdge = new Arc(v, w);
-			CustomAdjs[u].Add(newEdge);
+			double idd = Id;
+			Arc newEdge1 = new Arc(v, idd);
+			CustomAdjs[u].Add(newEdge1);
+			Edge newEdge = new Edge(u, v, Id);
+			this.EdgeList.Add(newEdge);
+			this.EdgeList.OrderBy(x => x.Id);
+			if (EdgeAdjs[u]==null) EdgeAdjs[u] = new List<Edge>();
+			this.EdgeAdjs[u].Add(newEdge);
 		}
+		public void AddEdge(int u, int v, int Id, int label)
+		{
+			double idd = Id;
+			Arc newEdge1 = new Arc(v, idd);
+			CustomAdjs[u].Add(newEdge1);
+			Edge newEdge = new Edge(u, v, Id, label);
+			this.EdgeList.Add(newEdge);
+			this.EdgeList.OrderBy(x => x.Id);
+			if (EdgeAdjs[u] == null) EdgeAdjs[u] = new List<Edge>();
+			this.EdgeAdjs[u].Add(newEdge);
+		}
+
+		//public void AddEdge(int u, int v, double w)
+		//{
+		//	Arc newEdge = new Arc(v, w);
+		//	CustomAdjs[u].Add(newEdge);
+		//}
 
 		//Print all Paths from source to destination
 		public void PrintAllPaths(int s, int d)
@@ -84,18 +146,201 @@ namespace GraphTool
 			int[] path = new int[V];
 			int pathIndex = 0;
 			//Call Graph Travesal algorithm
-			DepthFirstSearch(s,d,visited,path,pathIndex);
+			//DepthFirstSearch(s,d,visited,path,pathIndex);
+			//BFSPath(s, d);
+			EdgesBFSPath(s, d);
+			Console.WriteLine("-------------------------------------------------------------");
+			BFS(s, d);
 		}
 
 		#endregion
 		#region private
 		//Breath First Search		
-		void BFS(int s,bool[] visited, int[] path, int pathIndex){
+		public List<int>[] reachedBy { get; set; }
+		List<int[]> AllStackPath;		
+		void EdgesBFSPath(int s, int d)
+		{
+			//Init new Graph on Path
+			bool[] visitedEdge = new bool[this.EdgeList.Max(x => x.Id) + 1];
+			var startEdges = EdgeList.Where(x => x.StartNode == s);
+			if (startEdges == null) return;
+			foreach (var startEdge in startEdges)
+			{
+				Queue<List<Edge>> pathQueue = new Queue<List<Edge>>();
+				List<Edge> tempPath = new List<Edge>();
+				tempPath.Add(startEdge);
+				pathQueue.Enqueue(tempPath);
+				
+				Edge lastEdge;
+				int pathLenght = 1;
+				int currentLabel = startEdge.Label;
+				List<Edge> newPath;
+				while (pathQueue.Count > 0)
+				{
+					List<Edge> lastPath = pathQueue.Last();
+					tempPath = pathQueue.Dequeue();					
+					//if (tempPath.Count < lastPath.Count) continue;
+					lastEdge = tempPath.Last();
+					currentLabel = lastEdge.Label;
+					visitedEdge[lastEdge.Id] = true;
+					//PrintTempPath(tempPath);
+					if (lastEdge.EndNode == d)
+					{
+						Console.Write("--------------------------------------------------");
+						Console.Write("good path ");
+						PrintTempPath(tempPath);
+					}
+					else
+					{
+						var nextEdgeCheck = EdgeAdjs[lastEdge.EndNode].FirstOrDefault(x => x.Label == currentLabel);
+						if (nextEdgeCheck != null)
+						{
+							newPath = new List<Edge>(tempPath);
+							newPath.Add(nextEdgeCheck);
+							pathLenght++;
+							pathQueue.Enqueue(newPath);
+						}
+						else
+						{
+							foreach (var adjEdge in EdgeAdjs[lastEdge.EndNode])
+							{
+								if (!tempPath.Contains(adjEdge))
+								{
+									newPath = new List<Edge>(tempPath);
+									newPath.Add(adjEdge);
+									pathLenght++;
+									pathQueue.Enqueue(newPath);
+								}
+							}
+						}						
+					}
+				}
+			}
+			
 
+		}
+
+		void BFSPath(int s, int d)
+		{
+			//Init new Graph on Path
+			List<Arc>[] LocalAdjs = new List<Arc>[V];
+
+			Queue<List<int>> pathQueue = new Queue<List<int>>();
+			reachedBy = new List<int>[V];
+			List<int> tempPath = new List<int>();
+			tempPath.Add(s);
+			pathQueue.Enqueue(tempPath);
+			int lastNode;
+			while (pathQueue.Count > 0)
+			{
+				tempPath = pathQueue.Dequeue();
+				lastNode = tempPath.Last();
+				PrintTempPath(tempPath);
+				if (lastNode == d)
+				{
+					Console.Write("--------------------------------------------------");
+					Console.Write("good path ");
+					PrintTempPath(tempPath);
+				}
+				else
+				{
+					foreach (var adjEdge in EdgeAdjs[lastNode])
+					{
+						int adjNode = adjEdge.EndNode;
+						if (!tempPath.Contains(adjNode))
+						{
+							List<int> newPath = new List<int>(tempPath);
+							newPath.Add(adjNode);
+							pathQueue.Enqueue(newPath);
+						}
+					}
+				}
+			}
+
+		}
+
+		private void PrintTempPath(List<Edge> tempPath)
+		{
+			Console.Write(tempPath[0].StartNode + ":" + tempPath[0].Label);
+			foreach (var item in tempPath)
+			{
+				Console.Write("{0}:{1} -> ", item.EndNode, item.Label);
+				//Console.Write("[{0},{1}]:{2} -> ", item.StartNode, item.EndNode, item.Id);
+				//Console.Write(" -> " + item.EndNode);
+			}
+			Console.WriteLine();
+		}
+
+		private void PrintTempPath(List<int> tempPath)
+		{
+			foreach (var item in tempPath)
+			{
+				Console.Write(item + "-> ");
+			}
+			Console.WriteLine();
+		}
+
+		void BFS(int u, int v)
+		{
+			AllStackPath = new List<int[]>();
+			reachedBy = new List<int>[V];
+			//List<int>[] path = new List<int>[V];
+			bool[] visited = new bool[V];
+			visited[u] = true;
+			mainQueue.Enqueue(u);
+			int pathLenght = 0;
+			while (mainQueue.Count > 0)
+			{
+				int current = mainQueue.Dequeue();
+				pathLenght++;
+				foreach (var adjEdge in CustomAdjs[current])
+				{
+					int adjNode = adjEdge.Des;
+					if (reachedBy[adjNode] == null) reachedBy[adjNode] = new List<int>();
+					reachedBy[adjNode].Add(current);
+					if (!visited[adjNode])
+					{
+						//mark visited this vertice
+						visited[adjNode] = true;
+						mainQueue.Enqueue(adjNode);
+					}
+				}
+
+			}
+
+			foreach (var item in PathTo(u,v,visited,reachedBy))
+			{
+				Console.Write(item + ":"+ this.EdgeAdjs[item][0].Label + " -> ");
+			}
+		}
+
+		private void tryPath(int pathIndex, int u, int v, bool[] visitedPath, Stack<int> stackPath)
+		{
+
+		}
+
+		public bool HasPathTo(bool[] visited, int v)
+		{
+			return visited[v];
+		}
+		public IEnumerable<int> PathTo(int s, int v, bool[] visited, List<int>[] path)
+		{
+			if (!HasPathTo(visited, v)) return null;
+			Stack<int> stackpath = new Stack<int>();
+			for (int x = v; x != s; x = path[x][0])
+				stackpath.Push(x);
+			stackpath.Push(s);
+			return stackpath;
 		}
 
 
 		//Depth First Search
+
+		void DFSUsingStack(int u, int d)
+		{
+
+		}
+
 		void DepthFirstSearch(int u, int d, bool[] visited, int[] path, int pathIndex)
 		{
 			//Mark visited current vertex (u)
@@ -131,12 +376,12 @@ namespace GraphTool
 		private void PrintPathAndDistanceToList(int[] path, int pathIndex)
 		{
 			double sum = 0;
-			for (int i = 0;i< pathIndex-1; i++)
+			for (int i = 0; i < pathIndex - 1; i++)
 			{
 				int currentVertex = path[i];
 				int nextVertex = path[i + 1];
 				//Reminde CustomAdjs[currentVertex] list all adjacency of currentVertex
-				sum += CustomAdjs[currentVertex].Find(x=>x.Des == nextVertex).Weight;
+				sum += CustomAdjs[currentVertex].Find(x => x.Des == nextVertex).Weight;
 			}
 			Tuple<List<int>, double> newTuple = new Tuple<List<int>, double>(path.Take(pathIndex).ToList(), sum);
 			this.AllPathListWithDistance.Add(newTuple);
@@ -153,3 +398,4 @@ namespace GraphTool
 
 	}
 }
+
--- GraphTool/GraphTool/Program.cs
+++ GraphTool/GraphTool/Program.cs
@@ -10,49 +10,51 @@ namespace GraphTool
 	{
 		static void Main(string[] args)
 		{
-			Graph g = new Graph(601);
-			g.AddEdge(0, 1, 3);
-			g.AddEdge(0, 1, 3);
-			g.AddEdge(0, 1, 3);
-			g.AddEdge(0, 2, 2);
-			g.AddEdge(0, 3, 2);
-			g.AddEdge(2, 0, 5);
-			g.AddEdge(2, 0, 5);
-			g.AddEdge(2, 0, 5);
-			g.AddEdge(2, 0, 5);
-			g.AddEdge(2, 1, 8);
-			g.AddEdge(1, 3, 5);
-			g.AddEdge(1, 3, 5);
-			g.AddEdge(1, 3, 5);
-			g.AddEdge(1, 3, 5);
-			for (int i = 0; i < 599; i++)
+			Graph g = new Graph(1700);
+			//g.AddEdge(0, 1, 1);
+			//g.AddEdge(0, 1, 7);
+			//g.AddEdge(1, 2, 2);
+			//g.AddEdge(1, 2, 8);
+			//g.AddEdge(2, 3, 3);
+			//g.AddEdge(1, 3, 9);
+			//g.AddEdge(3, 4, 4);
+			//g.AddEdge(4, 5, 5);
+			//g.AddEdge(5, 0, 6);
+			string[] lines = System.IO.File.ReadAllLines(@"D:\test.txt");
+			foreach (var line in lines)
 			{
-				Random rdm = new Random();
-				g.AddEdge(i, i + 1, rdm.Next(1, 100));
-				g.AddEdge(i, i + 1, rdm.Next(1, 100));
-				g.AddEdge(i, i + 1, rdm.Next(1, 100));
-				g.AddEdge(i + 1, i, rdm.Next(1, 100));
+				var items = line.Split();
+				g.AddEdge(int.Parse(items[0]), int.Parse(items[1]), int.Parse(items[2]), int.Parse(items[3]));
 			}
-			//string[] lines = System.IO.File.ReadAllLines(@"D:\WriteLines.txt");
-			//foreach(var line in lines)
-			//{
-			//	var items = line.Split();
-			//	g.AddEdge(int.Parse(items[0]), int.Parse(items[1]), double.Parse(items[2]));
-			//}
 
-			int s = 2, d = 500;
+			int s = 3, d = 101;
 			g.PrintAllPaths(s, d);
-			Console.WriteLine("Lisstttt");
-			g.AllPathList.ForEach(x => Console.WriteLine(String.Join(" ",x)));
-			Console.WriteLine("Custom lissstttttt");			
-			foreach (var item in g.AllPathListWithDistance)
-			{
-				item.Item1.ForEach(i => Console.Write("{0}\t", i));
-				Console.WriteLine();
-				Console.WriteLine("Total cost = " + item.Item2);
-				Console.WriteLine("----------------------------------");
-			}
+			//var test = g.BFSAllPath;
+			
+			// Console.WriteLine("Lisstttt");
+			// g.AllPathList.ForEach(x => Console.WriteLine(String.Join(" ",x)));
+			// Console.WriteLine("Custom lissstttttt");			
+			// foreach (var item in g.AllPathListWithDistance)
+			// {
+			// 	item.Item1.ForEach(i => Console.Write("{0}\t", i));
+			// 	Console.WriteLine();
+			// 	Console.WriteLine("Total cost = " + item.Item2);
+			// 	Console.WriteLine("----------------------------------");
+			// }
 			Console.Read();
+
+
+
+
+			// for (int i = 0; i < 599; i++)
+			// {
+			// 	Random rdm = new Random();
+			// 	g.AddEdge(i, i + 1, rdm.Next(1, 100));
+			// 	g.AddEdge(i, i + 1, rdm.Next(1, 100));
+			// 	g.AddEdge(i, i + 1, rdm.Next(1, 100));
+			// 	g.AddEdge(i + 1, i, rdm.Next(1, 100));
+			// }
+			
 		}
 	}
 }
